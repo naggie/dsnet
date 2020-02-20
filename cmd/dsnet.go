b600d147fec049d2cb0c82b0b03e929fb6a5d73d
Callan Bryant <callan.bryant@gmail.com>
2020-02-20 20:02:56 +0000

    + main cmd switch

--- cmd/dsnet.go
+++ cmd/dsnet.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"os"
+	"flag"
 	"github.com/naggie/dsnet"
 )
 
 func main() {
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 
+
+	switch os.Args[1] {
+		case "init":
+
+		case "up":
+
+		case "add":
+
+		case "report":
+
+		case "down":
+
+		default:
+			help();
+	}
 }
