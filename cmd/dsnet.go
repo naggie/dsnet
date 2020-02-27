7a2dc1098eead28406a55150b08951ea6ab5aa54
Callan Bryant <callan.bryant@gmail.com>
2020-02-27 23:14:04 +0000

    add report cmd to help

--- cmd/dsnet.go
+++ cmd/dsnet.go
@@ -29,16 +29,19 @@ func main() {
 }
 
 func help() {
-	fmt.Printf(`Usage: dsnet <cmd>
+	fmt.Printf(`dsnet is a simple tool to manage a wireguard VPN.
+
+Usage: dsnet <cmd>
 
 Available commands:
 
-	init : Create %s containing default configuration + new keys without loading. Edit to taste.
-	add  : Generate configuration for a new peer. (Send with passworded ffsend)
-	sync : Synchronise wireguard configuration with %s, creating and activating interface if necessary
+	init   : Create %s containing default configuration + new keys without loading. Edit to taste.
+	add    : Generate configuration for a new peer. (Send with passworded ffsend)
+	sync   : Synchronise wireguard configuration with %s, creating and activating interface if necessary
+	report : Generate a JSON status report to the location configured in %s
 
 To remove an interface or bring it down, use standard tools such as iproute2.
 To modify or remove peers, edit %s and then run sync.
 
-`, dstask.CONFIG_FILE, dstask.CONFIG_FILE, dstask.CONFIG_FILE)
+`, dstask.CONFIG_FILE, dstask.CONFIG_FILE, dstask.CONFIG_FILE, dstask.CONFIG_FILE)
 }
