3cce9fa63ebef53ef40e7973b6c9a7114ca39192
Callan Bryant <callan.bryant@gmail.com>
2020-03-02 02:35:32 +0000

    start unmarshalling of config file

--- add.go
+++ add.go
@@ -1,5 +1,14 @@
 package dsnet
 
-func Add(hostname string, owner string, description string, publicKey string) {
+import (
+	"io/ioutil"
+	"encoding/json"
+)
 
+func Add(hostname string, owner string, description string, publicKey string) {
+	raw, err := ioutil.ReadFile(CONFIG_FILE)
+	check(err)
+	conf := DsnetConfig{}
+	err = json.Unmarshal(raw, &conf)
+	check(err)
 }
