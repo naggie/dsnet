7742c96793cfc5106412aff8bd04b2926bf2f09a
Marvin Steadfast <marvin@xsteadfastx.org>
2020-12-30 14:46:57 +0100

    starting to use cobra for command line interface

--- cmd/dsnet.go
+++ cmd/dsnet.go
@@ -2,71 +2,100 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 
 	"github.com/naggie/dsnet"
+	"github.com/spf13/cobra"
+)
+
+var (
+	// Flags.
+	outputType string
+
+	// Commands.
+	rootCmd = &cobra.Command{}
+
+	initCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Init()
+		},
+		Use: "init",
+		Short: fmt.Sprintf(
+			"Create %s containing default configuration + new keys without loading. Edit to taste.",
+			dsnet.CONFIG_FILE,
+		),
+	}
+
+	addCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Add()
+		},
+		Use:   "add",
+		Short: "Add a new peer + sync",
+	}
+
+	upCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Up()
+		},
+		Use:   "up",
+		Short: "Create the interface, run pre/post up, sync",
+	}
+
+	syncCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Sync()
+		},
+		Use:   "sync",
+		Short: fmt.Sprintf("Update wireguard configuration from %s after validating", dsnet.CONFIG_FILE),
+	}
+
+	reportCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Report()
+		},
+		Use:   "report",
+		Short: fmt.Sprintf("Generate a JSON status report to the location configured in %s.", dsnet.CONFIG_FILE),
+	}
+
+	removeCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Remove()
+		},
+		Use:   "remove",
+		Short: "Remove a peer by hostname provided as argument + sync",
+	}
+
+	downCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			dsnet.Down()
+		},
+		Use:   "down",
+		Short: "Destroy the interface, run pre/post down",
+	}
+
+	versionCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("dsnet version %s\ncommit %s\nbuilt %s", dsnet.VERSION, dsnet.GIT_COMMIT, dsnet.BUILD_DATE)
+		},
+		Use:   "version",
+		Short: "Print version",
+	}
 )
 
 func main() {
-	var cmd string
+	rootCmd.PersistentFlags().StringVar(&outputType, "output", "wg-quick", "config file format: vyatta/wg-quick/nixos")
 
-	if len(os.Args) == 1 {
-		cmd = "help"
-	} else {
-		cmd = os.Args[1]
+	rootCmd.AddCommand(initCmd)
+	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(upCmd)
+	rootCmd.AddCommand(syncCmd)
+	rootCmd.AddCommand(reportCmd)
+	rootCmd.AddCommand(removeCmd)
+	rootCmd.AddCommand(downCmd)
+	rootCmd.AddCommand(versionCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
 	}
-
-	switch cmd {
-	case "init":
-		dsnet.Init()
-
-	case "add":
-		dsnet.Add()
-
-	case "up":
-		dsnet.Up()
-
-	case "sync":
-		dsnet.Sync()
-
-	case "report":
-		dsnet.Report()
-
-	case "remove":
-		dsnet.Remove()
-
-	case "down":
-		dsnet.Down()
-
-	default:
-		help()
-	}
-}
-
-func help() {
-	fmt.Printf(`dsnet is a simple tool to manage a centralised wireguard VPN.
-
-Usage: dsnet <cmd>
-
-Available commands:
-
-	init   : Create %[1]s containing default configuration + new keys without loading. Edit to taste.
-	add    : Add a new peer + sync
-	up     : Create the interface, run pre/post up, sync
-	report : Generate a JSON status report to the location configured in %[1]s.
-	remove : Remove a peer by hostname provided as argument + sync
-	down   : Destroy the interface, run pre/post down
-	sync   : Update wireguard configuration from %[1]s after validating
-
-Environment variables:
-
-	DSNET_OUTPUT=wg-quick : The add command will generate a wg-quick config (default)
-	DSNET_OUTPUT=vyatta   : The add command will generate a vyatta (edgeos) config
-	DSNET_OUTPUT=nixos    : The add command will generate a NixOS config
-
-dsnet version %[2]s
-commit %[3]s
-built %[4]s
-
-`, dsnet.CONFIG_FILE, dsnet.VERSION, dsnet.GIT_COMMIT, dsnet.BUILD_DATE)
 }
